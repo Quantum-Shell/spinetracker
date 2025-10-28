569807811f5dbfc260db768fc9d88fbd0d8fa7ad
TurtleWake <90726868+TurtleWake@users.noreply.github.com>
2025-10-28 11:20:57 -0400

    Adding navigation and corresponding pages

--- frontend/app/tabs/_layout.tsx
+++ frontend/app/tabs/_layout.tsx
@@ -1,10 +1,62 @@
 import { Tabs } from 'expo-router';
 
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="about" options={{ title: 'About' }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#ffd33d',
+        headerStyle: {
+          backgroundColor: '#25292e',
+        },
+        headerShadowVisible: false,
+        headerTintColor: '#fff',
+        tabBarStyle: {
+          backgroundColor: '#25292e',
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Shelf',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'library-sharp' : 'library-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="tracker"
+        options={{
+          title: 'Tracker',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'stats-chart' : 'stats-chart-outline'} color={color} size={24} />
+
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="discover"
+        options={{
+          title: 'Discover',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'search' : 'search-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'settings-sharp' : 'settings-outline'} color={color} size={24} />
+          ),
+        }}
+      />
     </Tabs>
+
+    
   );
 }
--- frontend/app/tabs/discover.tsx
+++ frontend/app/tabs/discover.tsx
@@ -1,9 +1,9 @@
 import { Text, View, StyleSheet } from 'react-native';
 
-export default function Index() {
+export default function DiscoverScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Books, books, and more books!</Text>
+      <Text style={styles.text}>We discover things here</Text>
     </View>
   );
 }
@@ -12,15 +12,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
     color: '#fff',
   },
 });
